668efa7a82a4f8aebf5def2bf877a89d82df6e2a
Guilherme Henrique Draghetti <guilherme.draghetti@acad.pucrs.br>
2019-05-27 17:57:20 -0300

    Inicio do T2 de Sistemas Operarcionais

---
+++ GerenciadorDeMemoria/src/Manager/App.java
@@ -0,0 +1,6 @@
+package Manager;
+
+//lê o arquivo texto e cria os objetos com as solicitações;
+public class App {
+
+}
---
+++ GerenciadorDeMemoria/src/Manager/Solicitacao.java
@@ -0,0 +1,7 @@
+package Manager;
+
+//recebe os dados das solicitações, como tamanho da alocação ou liberação e qual bloco;
+//Cria os nodos que estarão na LinkedList;
+public class Solicitacao {
+
+}
