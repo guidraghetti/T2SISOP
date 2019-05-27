53575c11f7fde31a28353067f616e627154ed9ab
Guilherme Henrique Draghetti <guilherme.draghetti@acad.pucrs.br>
2019-05-27 18:06:07 -0300

    Inicio da leitura de arquivo

--- GerenciadorDeMemoria/src/Manager/App.java
+++
@@ -1,6 +0,0 @@
-package Manager;
-
-//lê o arquivo texto e cria os objetos com as solicitações;
-public class App {
-
-}
--- GerenciadorDeMemoria/src/Manager/Solicitacao.java
+++
@@ -1,7 +0,0 @@
-package Manager;
-
-//recebe os dados das solicitações, como tamanho da alocação ou liberação e qual bloco;
-//Cria os nodos que estarão na LinkedList;
-public class Solicitacao {
-
-}
